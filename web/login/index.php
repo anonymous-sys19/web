29b61402e205c100f7d7318a8f42c99b8d6ea8f6
anonymous-sys19 <70624994+anonymous-sys19@users.noreply.github.com>
2021-06-29 03:56:21 +0200

    Add files via upload

---
+++ web/login/index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <title>Login Dark Hacker</title>
+
+</head>
+
+<body>
+
+
+<div class="logo"> 
+<h1><marquee >🔥🔥 𝖂𝖊𝖑𝖈𝖔𝖒𝖊 𝕯𝖆𝖗𝖐 𝕳𝖆𝖈𝖐𝖊𝖗 🔥🔥</marquee></h1>
+</div> 
+
+
+<section class="stark-login">
+  <form method="post" name="datosDeUsuario" id="datosDeUsuario">	
+    <div id="fade-box">
+      <input type="text" name="name" id="name" placeholder="Nombre completo">
+      <input type="email" name="email" id="email" placeholder="Email">
+      <input type="submit" id="registrar" name="registrar" value="register"> 
+    </div>
+    
+  </form>
+<?php
+  include("database.php");
+?>
+</section> 
+
+</html>
+
---
+++ web/main.js
@@ -0,0 +1,11 @@
+let ubicacionPrincipal = Window.pageYOffset;
+
+window.onscroll = function() {
+    let desplasamiento = window.pageYOffset;
+    if (ubicacionPrincipal >= desplasamiento) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-100px";
+    } 
+    ubicacionPrincipal = desplasamiento; 
+}
