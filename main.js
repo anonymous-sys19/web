cb453dcaa81273b7725999369b7d2e025e5c2284
anonymous-sys19 <estudiosrg5@gmail.com>
2023-03-08 17:52:54 -0600

    Gh-pages new pull

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 let ubicacionPrincipal = Window.pageYOffset;
 
-window.onscroll = function() {
+window.onscroll = function () {
     let desplasamiento = window.pageYOffset;
     if (ubicacionPrincipal >= desplasamiento) {
         document.getElementById("header").style.top = "0";
     } else {
         document.getElementById("header").style.top = "-100px";
-    } 
-    ubicacionPrincipal = desplasamiento; 
+    }
+    ubicacionPrincipal = desplasamiento;
 }
 
 
+
+function doct() {
+
+    document.querySelector('.alertPHP').addEventListener("click", clickPHP)
+    function clickPHP() {
+        data = false
+        if (data) {
+            return
+        }
+        alert("Error: pagina en desarrollo(PHP IS NOT SOPORTD)")
+    }
+}
